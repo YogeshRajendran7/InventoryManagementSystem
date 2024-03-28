36927f835bb12eedbbe0c534bc53de5e7d4d07ef
Rajendran <yogesh.a.rajendran@accenture.com>
2024-03-28 13:27:18 +0530

    Added Classes and methods

---
+++ InventoryManagementSystem/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+
+        public Book(string isbn, string title, string author, double price)
+        {
+            ISBN = isbn;
+            Title = title;
+            Author = author;
+            Price = price;
+        }
+    }
+}
---
+++ InventoryManagementSystem/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    class Inventory
+    {
+        private List<Book> books = new List<Book>();
+
+        public void AddBook(string isbn, string title, string author, double price)
+        {
+            Book book = new Book(isbn, title, author, price);
+            books.Add(book);
+            Console.WriteLine("Book added successfully!");
+        }
+
+        public void UpdateBook(string isbn)
+        { 
+           
+
+        }
+
+        public void ListBook()
+        {
+
+        }
+    }
+}
